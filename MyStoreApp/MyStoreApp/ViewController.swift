6a24de976f3dfde1f452bad779d748c1a4f28b33
Ali <alisham93@bk.ru>
2022-10-07 03:17:04 +0200

    First commit

---
+++ MyStoreApp/MyStoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStoreApp
+//
+//  Created by Эл on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
